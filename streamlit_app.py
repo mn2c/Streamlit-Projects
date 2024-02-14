1996763cdce647cae8b3a2ff9c656974b45250a3
mn2c <martinnchee@gmail.com>
2024-02-14 16:38:10 +0800

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+import requests
+import numpy as np
+import pandas as pd
+import streamlit as st
+import sqlalchemy, json, psycopg2
+
+
+
+st.write('Hello world, hello there')
+
+engine = sqlalchemy.create_engine(st.secrets["heroku_uri"], paramstyle="format")
+con = engine.raw_connection()
+curs = con.cursor()
+df_yield = pd.read_sql('SELECT * FROM yield_geo_pred LIMIT 100', con=engine)
+st.write(df_yield)
+
+
+# url = 'https://jsonplaceholder.typicode.com/users'
+# data = requests.get(url).json()
+# df = pd.DataFrame(data)
+# st.write(df)
