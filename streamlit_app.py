1db096bf1ecb1bd3f65697815478314cdcc8b0c5
mn2c <martinnchee@gmail.com>
2024-02-15 10:13:08 +0800

    st connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,23 @@ import requests
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sqlalchemy
-import psycopg
+# import sqlalchemy
+# import psycopg_binary
 
-
+# see https://docs.streamlit.io/knowledge-base/tutorials/databases/postgresql
 
 st.write('Hello world, hello there')
 
-engine = sqlalchemy.create_engine(st.secrets["heroku_uri"], paramstyle="format")
-con = engine.raw_connection()
-curs = con.cursor()
-df_yield = pd.read_sql('SELECT * FROM yield_geo_pred LIMIT 100', con=engine)
-con.close()
-st.write(df_yield)
+conn = st.connection("postgresql", type="sql")
+df = conn.query('SELECT * FROM test3 LIMIT 10;', ttl="10m")   #query results is cached no longer than 10 minutes
+st.write(df)
+
+# engine = sqlalchemy.create_engine(st.secrets["heroku_uri"], paramstyle="format")
+# con = engine.raw_connection()
+# curs = con.cursor()
+# df_yield = pd.read_sql('SELECT * FROM test3 LIMIT 10', con=engine)
+# con.close()
+# st.write(df_yield)
 
 
 # url = 'https://jsonplaceholder.typicode.com/users'
