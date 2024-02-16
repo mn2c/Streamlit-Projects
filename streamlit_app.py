653d153a8d7b647ff68d21984bde639e921534bc
mn2c <martinnchee@gmail.com>
2024-02-16 17:01:50 +0800

    openweather api

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,23 @@ import requests
 import numpy as np
 import pandas as pd
 import streamlit as st
-# import sqlalchemy
-# import psycopg_binary
-
-# see https://docs.streamlit.io/knowledge-base/tutorials/databases/postgresql
 
 st.write('Hello world, hello there')
 
-conn = st.connection("postgresql", type="sql")
-df = conn.query('SELECT * FROM test3 LIMIT 10;', ttl="10m")   #query results is cached no longer than 10 minutes
-st.write(df)
-
-# engine = sqlalchemy.create_engine(st.secrets["heroku_uri"], paramstyle="format")
-# con = engine.raw_connection()
-# curs = con.cursor()
-# df_yield = pd.read_sql('SELECT * FROM test3 LIMIT 10', con=engine)
-# con.close()
-# st.write(df_yield)
-
+# conn = st.connection("postgresql", type="sql")
+# df = conn.query('SELECT * FROM test3 LIMIT 10;', ttl="10m")   #query results is cached no longer than 10 minutes
+# st.write(df)
 
 # url = 'https://jsonplaceholder.typicode.com/users'
 # data = requests.get(url).json()
 # df = pd.DataFrame(data)
 # st.write(df)
+
+params = {
+    "q": "London,uk",
+    "APPID": st.secrets["OpenWeatherAppId"]
+}
+url = 'https://api.openweathermap.org/data/2.5/weather'
+response = requests.get(url, params=params)
+data = response.json()
+st.write("Temperature:", data["main"]["temp"], "K")
